397d19010ddca1802bdb11454d6fd36a41ebb47a
Gowra Pavan Kumar <160145497+gowrapavan@users.noreply.github.com>
2025-09-26 23:44:43 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from difflib import SequenceMatcher  # fuzzy string matching
 
 # ---------------- CONFIG ---------------- #
-API_KEY = "c4f418216060fd71ea5505368b40f232"
+API_KEY = "4c67514f5388361ab34343e62c8e13df"
 BASE_URL = "https://v3.football.api-sports.io/fixtures"
 HEADERS = {"x-apisports-key": API_KEY}
 
