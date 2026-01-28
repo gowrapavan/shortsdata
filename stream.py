52b3e7eb3eabc05ff148af7116cbad70952977b4
Gowra Pavan Kumar <160145497+gowrapavan@users.noreply.github.com>
2026-01-28 14:53:50 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -53,6 +53,8 @@ TEAM_SOURCES = {
     "ITSA": "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/teams/ITSA.json",
     "DED": "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/teams/DED.json",
     "DEB": "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/teams/DEB.json",
+    "UCL": "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/teams/UCL.json",
+    "WC": "https://raw.githubusercontent.com/gowrapavan/shortsdata/main/teams/WC.json",
 }
 
 TEAM_DATA = []
